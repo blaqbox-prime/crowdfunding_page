a5127761bbd734df7b6e1eadcd5e53d211bafc7b
Karabo Sambo <karabosambo@outlook.com>
2022-04-26 12:10:37 +0200

    remove build folder from git ignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { extendTheme } from "@chakra-ui/react"
+
+// Set Theme Values
+const theme = extendTheme({
+  Colors: {
+      primary: {
+        moderate_cyan: "hsl(176, 50%, 47%)",
+        dark_cyan: "hsl(176, 72%, 28%)",
+      },
+      Neutral: {
+       dark_gray: "hsl(0, 0%, 48%)"
+      }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +23,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
